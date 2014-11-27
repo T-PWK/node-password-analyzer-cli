4f9c5af19eb8d929aa31dbe9b46c827be86f3db8
T-PWK <tompwk@gmail.com>
2014-11-27 19:39:58 +0000

    Executable renamed

--- bin/password-analyzer-cli.js
+++ bin/password-analyzer-cli.js
@@ -1,74 +1,55 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var chalk = require('chalk');
 var fs = require('fs');
 var pkg = require('../package.json');
 var analyzer = require('password-analyzer');
-var LineTransform = require('node-line-reader').LineTransform;
+
+var input, style = chalk.green;
 
 program
-  .version(pkg.version)
-  .usage('[options] <file>')
-  .option('-o, --output <file>', 'analysis output file')
-  .option('-c, --config <file>', 'configuration file')
-  .option('-n, --num <num>', 'number of passwords to analyze', parseInt, -1)
-  .parse(process.argv);
+	.version(pkg.version)
+	.usage('[options] <file>')
+	.option('-o, --output <file>', 'analysis output file')
+	.option('-c, --config <file>', 'configuration file')
+	.option('-n, --num <num>', 'number of passwords to analyse', parseInt, -1)
+	.parse(process.argv);
 
-verify();
+console.log(program)
 
-// Setup password analyzer
-var passwordAnalyzer = new analyzer.PasswordAnalyzer();
-
-passwordAnalyzer.addGroup('Character sets', ['numeric', 'loweralpha', 'upperalpha']);
-passwordAnalyzer.addGroup('Months', ['months']);
-
-// Setup input stream
-var stream = fs.createReadStream(program.args[0]);
-var transform = new LineTransform({ skipEmpty: true });
-
-stream.pipe(transform);
-
-transform.on('data', function (password) {
-  if (password.length === 0) return;
-  passwordAnalyzer.analyze(password);
-});
-
-transform.on('end', function () {
-  console.log(passwordAnalyzer.getResults());
-});
-
-console.log(",------.                                                   ,--.");
-console.log("|  .--. ' ,--,--. ,---.  ,---. ,--.   ,--. ,---. ,--.--. ,-|  |");
-console.log("|  '--' |' ,-.  |(  .-' (  .-' |  |.'.|  || .-. ||  .--'' .-. |");
-console.log("|  | --' \\ '-'  |.-'  `).-'  `)|   .'.   |' '-' '|  |   \\ `-' |");
-console.log("`--'      `--`--'`----' `----' '--'   '--' `---' `--'    `---'");
-console.log("  ,---.                  ,--.");
-console.log(" /  O  \\ ,--,--,  ,--,--.|  |,--. ,--.,-----. ,---. ,--.--.");
-console.log("|  .-.  ||      \\' ,-.  ||  | \\  '  / `-.  / | .-. :|  .--'");
-console.log("|  | |  ||  ||  |\\ '-'  ||  |  \\   '   /  `-.\\   --.|  |");
-console.log("`--' `--'`--''--' `--`--'`--'.-'  /   `-----' `----'`--' v." + pkg.version);
-console.log("                             `---'")
-
-
-function error () {
-  var args = [].slice.apply(arguments);
-  console.log('');
-  console.log.apply(console, args);
-  console.log('');
-  program.help();
+if(program.args.length === 0) {
+	program.help();
 }
 
-function verify () {
-  // Input verification
-  if(program.args.length === 0) {
-    error("  Error: no input file");
-  }
+if(program.args.length > 1) {
+	error('  Error: too many input files');
+}
 
-  if(program.args.length > 1) {
-    error("  Error: too many input files");
-  }
+if(program.config && !fs.existsSync(program.config)) {
+	error("  Error: configuration '%s' file does not exist", program.config);
+}
 
-  if(program.config && !fs.existsSync(program.config)) {
-    error("  Error: specified configuration file '%s' does not exist", program.config); 
-  }
+console.log(style(" ,------.                                                   ,--."));
+console.log(style(" |  .--. ' ,--,--. ,---.  ,---. ,--.   ,--. ,---. ,--.--. ,-|  |"));
+console.log(style(" |  '--' |' ,-.  |(  .-' (  .-' |  |.'.|  || .-. ||  .--'' .-. |"));
+console.log(style(" |  | --' \\ '-'  |.-'  `).-'  `)|   .'.   |' '-' '|  |   \\ `-' |"));
+console.log(style(" `--'      `--`--'`----' `----' '--'   '--' `---' `--'    `---'"));
+console.log(style("   ,---.                  ,--."));
+console.log(style("  /  O  \\ ,--,--,  ,--,--.|  |,--. ,--.,-----. ,---. ,--.--."));
+console.log(style(" |  .-.  ||      \\' ,-.  ||  | \\  '  / `-.  / | .-. :|  .--'"));
+console.log(style(" |  | |  ||  ||  |\\ '-'  ||  |  \\   '   /  `-.\\   --.|  |"));
+console.log(style(" `--' `--'`--''--' `--`--'`--'.-'  /   `-----' `----'`--'"));
+console.log(style("                              `---'"), chalk.white(' v.' + pkg.version))
+
+var analyzer = new analyzer.PasswordAnalyzer();
+console.log(analyzer.getResults());
+
+
+function error() {
+	var args = Array.prototype.slice.call(arguments, 0);
+	args[0] = chalk.red(args[0]);
+
+	console.log.apply(console, args);
+	program.help();
 }
